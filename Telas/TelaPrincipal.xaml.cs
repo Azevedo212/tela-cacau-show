8ee12db89e67aec4a5efa065c177a155c4673da8
Pedro Henrique Azevedo <pedrohxpa@hotmail.com>
2024-04-23 16:50:43 -0400

    quinto commit

--- Telas/TelaPrincipal.xaml.cs
+++ Telas/TelaPrincipal.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TELA
+namespace TELA.Telas
 {
     /// <summary>
-    /// Lógica interna para telaPrincipal.xaml
+    /// Interação lógica para TelaPrincipal.xam
     /// </summary>
-    public partial class telaPrincipal : Window
+    public partial class TelaPrincipal : Page
     {
-        public telaPrincipal()
+        public TelaPrincipal()
         {
             InitializeComponent();
         }
